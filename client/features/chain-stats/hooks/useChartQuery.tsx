5fc091dea859ad185e20d2b23e08bafa46bf9176
tom goriunov <tom@ohhhh.me>
2026-04-27 17:13:18 +0200

    Adjust scale in d3 to display correct resolution by week in stats (#3359)
    * Adjust scale in d3 to display correct resolution by week in stats
    
    Resolves #2701
    
    * fixes

--- client/features/chain-stats/hooks/useChartQuery.tsx
+++ client/features/chain-stats/hooks/useChartQuery.tsx
@@ -1,7 +1,8 @@
 import type { ChartDataPayloadLine, StatsIntervalIds } from '../types/client';
-import type { ChartResolution } from 'toolkit/components/charts/types';
+import { ChartResolution } from 'toolkit/components/charts/types';
 
 import useApiQuery from 'lib/api/useApiQuery';
+import { getDateLabel } from 'toolkit/components/charts/line/utils/getDateLabel';
 
 import { CHAIN_STATS_LINE_CHART } from '../stubs/charts';
 import { getDatesFromInterval } from '../utils/interval';
@@ -41,11 +42,17 @@ export default function useChartQuery({ id, resolution, interval, enabled = true
             resolutions: [ ],
           },
           data: data.chart.map((item) => {
+            const date = new Date(item.date);
+            const dateTo = item.date_to ? new Date(item.date_to) : undefined;
+            // For resolution greater than day, we have to use the date_to field to display X labels correctly
+            const dateFormatted = resolution === ChartResolution.DAY ? date : dateTo ?? date;
+
             return {
-              date: new Date(item.date),
-              date_to: new Date(item.date_to),
+              date: dateFormatted,
+              date_to: dateTo,
               value: Number(item.value),
               isApproximate: item.is_approximate,
+              dateLabel: getDateLabel(date, dateTo, resolution),
             };
           }),
         };
